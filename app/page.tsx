312278487455904de2802774327dd528f081b238
eddysantiagoo <eddysantiagogh@gmail.com>
2025-04-09 14:40:09 -0500

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,40 @@
-import { HeroSection } from "@/components/hero-section-dark";
-import Github from "@/components/logos/github";
 import { NavBar } from "@/components/nav-bar";
 import { CallToAction } from "@/components/sections/callToAction/page";
 import FeaturesSection from "@/components/sections/features/page";
 import { FooterSection } from "@/components/sections/footer/page";
 import { HeroSectionMain } from "@/components/sections/hero/page";
 import TrustedBySection from "@/components/sections/trustedby/default";
-import { Footer } from "@/components/ui/footer";
-import { ShimmerButton } from "@/components/ui/shimmer-button";
-import { Hexagon, Twitter } from "lucide-react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vibio | CRM Conversacional para WhatsApp, Instagram y Ventas Automatizadas",
+  description:
+    "Vibio es el CRM conversacional que te ayuda a automatizar WhatsApp, Instagram y más. Centraliza tus chats, optimiza la atención al cliente y aumenta tus ventas sin perder el toque humano.",
+  openGraph: {
+    title: "Vibio CRM - Vibio lo hace fácil",
+    description:
+      "Vibio es el CRM conversacional que te ayuda a automatizar WhatsApp, Instagram y más. Centraliza tus chats, optimiza la atención al cliente y aumenta tus ventas sin perder el toque humano.",
+    type: "website",
+    url: "https://vibio.com",
+    siteName: "Vibio CRM",
+    images: [
+      {
+        url: "/og.png", 
+        width: 1200,
+        height: 630,
+        alt: "Vibio CRM - Vibio lo hace fácil",
+      },
+    ],
+
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vibio CRM - Vibio lo hace fácil",
+    description:
+      "Vibio es el CRM conversacional que te ayuda a automatizar WhatsApp, Instagram y más. Centraliza tus chats, optimiza la atención al cliente y aumenta tus ventas sin perder el toque humano.",
+  },
+
+};
 
 export default function Home() {
   return (
