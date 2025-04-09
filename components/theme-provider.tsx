c5515ab97b30fd21bf49dc177f230ecb65f19b86
eddysantiagoo <eddysantiagogh@gmail.com>
2025-04-09 13:38:30 -0500

    feat: add mounted state to ThemeProvider for client-side rendering,, fix hydratation

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,10 +2,21 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
